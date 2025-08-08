0b250fd79e3c7e00f436f37249de16d7d6ca471b
Harshal Margaj <harshal.margaj12@gmail.com>
2025-08-08 17:25:00 +0530

    prisma schema update

--- actions/create-community.ts
+++ actions/create-community.ts
@@ -1,11 +1,32 @@
 "use server";
 
 import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export const create = async (formData: FormData) => {
+	const { userId } = await auth();
+
+	if (!userId) {
+		redirect("/signin");
+	}
+
+	// const existingUser = await db.user.findUnique({
+	// 	where: {
+	// 		userId: userId,
+	// 	},
+	// });
+
+	// if (!existingUser) {
+	// 	await db.user.create({
+	// 		data: {
+	// 			userId: userId,
+	// 		},
+	// 	});
+	// }
+
 	const name = formData.get("name") as string;
 	const description = formData.get("description") as string;
-	const userId = formData.get("userId") as string;
 
 	await db.community.create({
 		data: {
