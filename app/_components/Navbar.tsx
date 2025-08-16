067a443f24c0ab9efd9b6466733c4562d3eca468
Harshal Margaj <harshal.margaj12@gmail.com>
2025-08-16 10:09:56 +0530

    add sound on click to search bar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -14,6 +14,7 @@ import { useUser } from "@clerk/nextjs";
 import { CommandDialogDemo } from "@/components/search-dialog";
 import { useQuery } from "@tanstack/react-query";
 import { get } from "@/actions/get-communities";
+import { playSound } from "@/lib/PlaySound";
 
 const Navbar = () => {
 	const { user } = useUser();
@@ -32,7 +33,10 @@ const Navbar = () => {
 
 			<div className="flex justify-end items-center  gap-4 ">
 				<div
-					onClick={() => setOpen(!open)}
+					onClick={() => {
+						playSound();
+						setOpen(!open);
+					}}
 					className="border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 inline-flex items-center justify-between gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer p-2 w-[250px]"
 				>
 					<div className="text-neutral-400">
