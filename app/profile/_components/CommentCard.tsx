25b3abe8f06d77fc75bf31331182d886fb713f3a
Harshal Margaj <harshal.margaj12@gmail.com>
2025-08-20 21:06:53 +0530

    make light version of profile page

--- app/profile/_components/CommentCard.tsx
+++ app/profile/_components/CommentCard.tsx
@@ -12,7 +12,7 @@ interface CommentCardProps {
 
 const CommentCard = ({ comment }: CommentCardProps) => {
 	return (
-		<div className="border border-neutral-900 rounded-md p-4 space-y-2">
+		<div className="border dark:border-neutral-900 rounded-md p-4 space-y-2">
 			<div className="flex items-start gap-2">
 				<div className="border border-neutral-700 rounded-full p-1 shadow-sm shadow-amber-100/10">
 					<Handshake className={"text-amber-100"} size={25} />
@@ -21,16 +21,18 @@ const CommentCard = ({ comment }: CommentCardProps) => {
 					<div className="font-semibold">
 						{comment.post.community?.name}
 					</div>
-					<div className="text-neutral-400">
+					<div className="dark:text-neutral-400 text-gray-500">
 						{comment.author?.userName}
 					</div>
 				</div>
 				<Dot />
-				<div className="text-sm text-neutral-500">
+				<div className="text-sm dark:text-neutral-500 text-gray-500">
 					{comment.post.title}
 				</div>
 			</div>
-			<div>{comment.content}</div>
+			<div className="dark:text-white text-gray-500">
+				{comment.content}
+			</div>
 		</div>
 	);
 };
