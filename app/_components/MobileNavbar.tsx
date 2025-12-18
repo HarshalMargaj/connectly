7ba47b701b8bb2e902edf9f79e63a27f72cb924a
Harshal Margaj <harshal.margaj12@gmail.com>
2025-12-18 19:13:44 +0530

    install&implement: useOnClickOutside hook from usehook-ts

--- app/_components/MobileNavbar.tsx
+++ app/_components/MobileNavbar.tsx
@@ -1,13 +1,20 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { SignedOut, SignInButton } from "@clerk/nextjs";
 import { Ellipsis } from "lucide-react";
-import React, { useState } from "react";
+import React, { RefObject, useRef, useState } from "react";
+import { useOnClickOutside } from "usehooks-ts";
 
 const MobileNavbar = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const ref = useRef<HTMLDivElement>(null);
+
+	const closeDropdown = () => {
+		setIsOpen(false);
+	};
+
+	useOnClickOutside(ref as RefObject<HTMLElement>, closeDropdown);
 
 	return (
-		<div className="relative md:hidden">
+		<div className="relative md:hidden" ref={ref}>
 			<Ellipsis onClick={() => setIsOpen(!isOpen)} />
 			{isOpen && (
 				<div className="absolute top-10 right-1 bg-neutral-900 p-2 rounded-md w-[200px]">
